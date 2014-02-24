d5e4b3dc93cbdf7835778c801f708cef4f2fedb1
Lucas <llisboaa@outlook.com>
2014-02-24 14:13:40 -0300

    Validacoes
    Validacoes

---
+++ Validacoes/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using Microsoft.SqlServer.Server;
+using Validacoes.Models;
+using System.Globalization;
+using System.ComponentModel.DataAnnotations;
+
+namespace Validacoes.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contato()
+        {
+            var pessoa = new Pessoa();
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public ActionResult Contato(Pessoa pessoa)
+        {
+
+
+            //---- Converte a primeira letra da palavra em maiúscula--------
+            CultureInfo cInfo = Thread.CurrentThread.CurrentCulture;
+
+            pessoa.Cidade = cInfo.TextInfo.ToTitleCase(pessoa.Cidade);
+            pessoa.Nome = cInfo.TextInfo.ToTitleCase(pessoa.Nome);
+            pessoa.Sobrenome = cInfo.TextInfo.ToTitleCase(pessoa.Sobrenome);
+            //---------------------------------------------------------------
+
+            //------------ Validação da cidade ----------------------------------------------------------------------------
+            if (!(pessoa.Cidade.Equals("Ariquemes") || pessoa.Cidade.Equals("Jaru") || pessoa.Cidade.Equals("Vilhena")))
+            {
+                ModelState.AddModelError("Cidade", "erro");
+            }
+            //-------------------------------------------------------------------------------------------------------------
+
+            return ModelState.IsValid ? View("Dados", pessoa) : View(pessoa); // if model state is valid...
+        }
+
+        public ActionResult ValidarCPF(string Cpf) // Implementar código de validação remota
+        {
+            string ERcpf = @"^\d{3}\.\d{3}\.\d{3}-\d{2}$";
+
+            return Json("");
+        }
+
+        public ActionResult Dados(Pessoa pessoa)
+        {
+            return View(pessoa);
+        }
+
+    }
+}
---
+++ Validacoes/Models/Pessoa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+using System.Web.UI.WebControls.Expressions;
+
+namespace Validacoes.Models
+{
+    public class Pessoa
+    {   
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo sobrenome é obrigatório")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "Campo CPF é obrigatório")]
+        public string Cpf { get; set; }
+
+        // Campo validado por ModelState no HomeController!
+        [Required(ErrorMessage = "O campo cidade é obrigatório")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Campo email é obrigatório")]
+        [RegularExpression(@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$", ErrorMessage = "Email em formato inválido")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+    }
+}
